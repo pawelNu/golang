be308a58b1b6c4a49d1402c6cbd13841845d28ae
pawelNu <pnurzynski17@gmail.com>
2024-10-08 18:33:32 +0200

    go progress 2024-10-08

--- tutorial-my-project/src/main.go
+++ tutorial-my-project/src/main.go
@@ -56,6 +56,7 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerUsersCreate)
+	v1Router.Get("/users", apiCfg.handlerUsersGet)
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
