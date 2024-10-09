f11a6ecee179093bd16964211dd05d25553da507
pawelNu <pnurzynski17@gmail.com>
2024-10-09 17:24:17 +0200

    go progress 2024-10-09

--- tutorial-project/08-feedfollows/src/models.go
+++ tutorial-project/08-feedfollows/src/models.go
@@ -3,8 +3,8 @@ package main
 import (
 	"time"
 
+	"github.com/bootdotdev/projects/feedfollows/internal/database"
 	"github.com/google/uuid"
-	"github.com/pawelNu/rssagg/internal/database"
 )
 
 type User struct {
@@ -49,6 +49,32 @@ func databaseFeedsToFeeds(feeds []database.Feed) []Feed {
 	result := make([]Feed, len(feeds))
 	for i, feed := range feeds {
 		result[i] = databaseFeedToFeed(feed)
+	}
+	return result
+}
+
+type FeedFollow struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserID    uuid.UUID `json:"user_id"`
+	FeedID    uuid.UUID `json:"feed_id"`
+}
+
+func databaseFeedFollowToFeedFollow(feedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        feedFollow.ID,
+		CreatedAt: feedFollow.CreatedAt,
+		UpdatedAt: feedFollow.UpdatedAt,
+		UserID:    feedFollow.UserID,
+		FeedID:    feedFollow.FeedID,
+	}
+}
+
+func databaseFeedFollowsToFeedFollows(feedFollows []database.FeedFollow) []FeedFollow {
+	result := make([]FeedFollow, len(feedFollows))
+	for i, feedFollow := range feedFollows {
+		result[i] = databaseFeedFollowToFeedFollow(feedFollow)
 	}
 	return result
 }
